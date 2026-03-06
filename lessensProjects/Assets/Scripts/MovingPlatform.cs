6fc50a65730c328476b115433bf2d6d9470bc6ee
DeonsGaming <Deon.mob@gmail.com>
2026-03-06 10:32:52 +0100

    added moving platform that doesnt move

---
+++ lessensProjects/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject _top;
+    [SerializeField] private GameObject _bottem;
+    [SerializeField] private float _movementSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if () { }
+    }
+}
