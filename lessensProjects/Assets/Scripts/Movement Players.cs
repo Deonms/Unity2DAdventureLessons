050afed2a1dc7975f3f23477a4abb4400b9fc8a5
DeonsGaming <Deon.mob@gmail.com>
2026-03-06 11:41:38 +0100

    Changed Player Input

--- lessensProjects/Assets/Scripts/Movement Players.cs
+++ lessensProjects/Assets/Scripts/Movement Players.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class MovementPlayers : MonoBehaviour
 {
-    [SerializeField] private float _jumpPower = 10;
-    [SerializeField] private float _walkSpeed = 10;
+    [SerializeField] private float _jumpPower = 3f;
+    [SerializeField] private float _walkSpeed = 3f;
+    [SerializeField] private Player1Input _player1Input;
+    [SerializeField] private Player2Input _player2Input;
+
+    private void Start()
+    {
+        _player1Input.OnPlayerInputRecieve.AddListener(MovePlayerSideWays);
+        _player2Input.OnPlayerInputRecieve.AddListener(MovePlayerSideWays);
+    }
     public void MovePlayerSideWays(Vector2 direction)
     {
         transform.position += new Vector3(direction.x, direction.y, 0) * _walkSpeed * Time.deltaTime;
