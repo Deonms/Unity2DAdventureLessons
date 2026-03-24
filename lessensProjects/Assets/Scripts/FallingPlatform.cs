c2416a6f991e68aa93a5783b95c02df6cd2b480c
DeonsGaming <Deon.mob@gmail.com>
2026-03-24 10:07:25 +0100

    added the coords of where to respawn and where to die to the platform

--- lessensProjects/Assets/Scripts/FallingPlatform.cs
+++ lessensProjects/Assets/Scripts/FallingPlatform.cs
@@ -4,6 +4,10 @@ public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private string _playerTag = "Player";
     [SerializeField] private float _speed = 2f;
+    [SerializeField] private GameObject _platformToFall;
+    [SerializeField] private float _toRespawnX;
+    [SerializeField] private float _toRespawnY;
+    [SerializeField] private float _dieFromY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
