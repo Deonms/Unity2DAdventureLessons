b9beab5955d06247d5e98dc01484af6ed4871195
DeonsGaming <Deon.mob@gmail.com>
2026-03-24 11:39:08 +0100

    made the falling platform

--- lessensProjects/Assets/Scripts/FallingPlatform.cs
+++ lessensProjects/Assets/Scripts/FallingPlatform.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private string _playerTag = "Player";
-    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _fallspeed = 10f;
     [SerializeField] private GameObject _platformToFall;
     [SerializeField] private float _toRespawnX;
     [SerializeField] private float _toRespawnY;
     [SerializeField] private float _dieFromY;
+
+    private bool _isFalling = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,13 +20,28 @@ public class FallingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_isFalling)
+        {
+            transform.position += new Vector3(0, -1, 0) * _fallspeed * Time.deltaTime;
 
+            // Respawn platform once it falls past the die threshold
+            if (transform.position.y < _dieFromY)
+            {
+                transform.DetachChildren();
+                transform.position = new Vector3(_toRespawnX, _toRespawnY, transform.position.z);
+                _isFalling = false;
+            }
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag(_playerTag))
         {
             collision.transform.SetParent(transform); //set de speler een child als ze op de moving platform staan
+        }
+        if (collision.gameObject.CompareTag(_playerTag))
+        {
+            _isFalling = true;
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -33,4 +51,7 @@ public class FallingPlatform : MonoBehaviour
             collision.transform.SetParent(null); //haalt de speler weg van child van de moving platform omdat ze er niet meer op staan
         }
     }
+    
+        
+
 }
